a84d648911e6015b584498c66432c5de789f1c7c
ZAS0RIN <zoid.b@ya.ru>
2015-08-28 00:25:53 +0500

    modified:   app/api/views.py

--- app/api/views.py
+++ app/api/views.py
@@ -30,3 +30,33 @@ def get_news():
     return jsonify({
         'data': False
     })
+
+@api.route('/get_new')
+def get_new():
+    for i in Event.query.filter_by(is_archive=False).order_by(Event.date.desc()).all():
+        print datetime.utcnow().isoformat() > i.date.isoformat()
+        print (datetime.utcnow() + timedelta(minutes=10)).isoformat() < i.date.isoformat()
+        if not datetime.utcnow().isoformat() > i.date.isoformat() and not (
+                    datetime.utcnow() + timedelta(minutes=10)).isoformat() < i.date.isoformat():
+            return jsonify({
+                'time': i.date.strftime("%Y-%m-%d %H:%M:%S"),
+                'title': 'Скоро начнется матч',
+                'desc': 'Играют' + i.get_name_event() + ', успевайте сделать ставки!',
+                'data': True
+            })
+    return jsonify({
+                'quoteLink': Event.query.all()[0].date.strftime("%Y-%m-%d %H:%M:%S"),
+                'quoteAuthor': 'Скоро начнется матч',
+                'quoteText': 'Играют' + i.get_name_event() + ', успевайте сделать ставки!',
+                'data': True
+            })
+    return jsonify({
+        'data': False
+    })
+
+
+@api.route('/get_ne')
+def get_ne():
+    return jsonify({
+        'data': False
+    })

7114e21a0b32c9ff87d05ede0f2e3debee3c317d
ZAS0RIN <zoid.b@ya.ru>
2015-08-28 00:49:09 +0500

    modified:   app/api/views.py

--- app/api/views.py
+++ app/api/views.py
@@ -17,15 +17,9 @@ def get_news():
         if not datetime.utcnow().isoformat() > i.date.isoformat() and not (
                     datetime.utcnow() + timedelta(minutes=10)).isoformat() < i.date.isoformat():
             return jsonify({
-                'time': i.date.strftime("%Y-%m-%d %H:%M:%S"),
-                'title': 'Скоро начнется матч',
-                'desc': 'Играют' + i.get_name_event() + ', успевайте сделать ставки!',
-                'data': True
-            })
-    return jsonify({
-                'quoteLink': 'http://forismatic.com/en/ef1bfe522b/',
-                'quoteAuthor': 'title',
-                'quoteText': 'text'
+                #'time': i.date.strftime("%Y-%m-%d %H:%M:%S"),
+                'quoteAuthor': 'Скоро начнется матч',
+                'quoteText': 'Играют ' + i.get_name_event() + ', успевайте сделать ставки!',
             })
     return jsonify({
         'data': False
